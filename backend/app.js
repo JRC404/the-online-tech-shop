3e729fbf44b09c5d1dc5c1546a1d329bb76648eb
PedroVPSN <pedrosilvavpsn@gmail.com>
2020-10-29 12:41:04 +0000

    routes and middleware added.

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,26 @@
 const express = require('express'); // import express
-const app = express();
+const mongoose = require('mongoose');
 require('dotenv').config(); // now we can use dotenv to run our server
 
-app.get('/', (req, res) => {
-    res.send("Server test");
-});
+// import routes
+const userRoutes = require('./routes/user')
+
+// app
+const app = express();
+
+// database
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("DB Connected"))
+
+// routes middleware
+app.use('/api', userRoutes)
 
 const port = process.env.PORT || 8000 
 
 app.listen(port, ()=> {
-    console.log(`Server is tunning on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
