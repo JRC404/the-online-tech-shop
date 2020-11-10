bce29b8dc269e1251b798f21f70e7a9238c91ded
Alex Coop <alexcoop4@gmail.com>
2020-11-10 17:05:43 +0000

    made sure correct express-valiudator is being used

--- app.js
+++ app.js
@@ -24,13 +24,12 @@ mongoose.connect(process.env.DATABASE, {
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-//app.use(expressValidator());
-
+app.use(expressValidator());
 
 // routes middleware
 app.use('/api', userRoutes);
 
-const port = process.env.PORT || 8000 
+const port = process.env.PORT || 8000; 
 
 app.listen(port, ()=> {
     console.log(`Server is running on port ${port}`);
