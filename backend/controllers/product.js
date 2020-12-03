6d8a7e3621ed9c3c2c4fefe32492207d5a545fe4
PedroVPSN <pedrosilvavpsn@gmail.com>
2020-12-03 23:17:16 +0000

    product by id and read product methods

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -4,6 +4,24 @@ const formidable = require('formidable');
 const _ = require('lodash');
 const fs = require('fs');
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if(err || !product) {
+            return res.status(400).json({
+                error: "Product not found."
+            });
+        };
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
+
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
