41a8828e7200b78e11aa8488f9a09f235f64c91b
PedroVPSN <pedrosilvavpsn@gmail.com>
2020-12-07 22:59:44 +0000

    remove and update methds

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -65,3 +65,62 @@ exports.create = (req, res) => {
         });
     });
 };
+
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, DeletedProduct) => {
+        if(err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        res.json({
+            "message": "Product deleted."
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if(err) {
+            return res.status(400).json({
+                error: 'Image could not be upload'
+            });
+        };
+
+        // check all fields
+        const { name, description, price, category, quantity, shipping } = fields;
+
+        if(!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: "All fields are require."
+            });
+        };
+
+        let product = req.product
+        product = _.extend(product, fields);
+
+        // max photo size 1mb
+        if(files.photo) {
+            if(files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: "Image should be less than 1mb in size"
+                });
+            };
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        };
+
+        product.save((err, result) => {
+          if(err) {
+            console.log("product create error", err)
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+          };
+          res.json(result);  
+        });
+    });
+};
